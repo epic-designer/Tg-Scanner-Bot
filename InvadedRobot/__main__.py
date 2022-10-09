04268f33fc68c2182c1482dad7fa6bbab3768d1b
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-09 15:09:34 +0530

    The perfect pm start

--- InvadedRobot/__main__.py
+++ InvadedRobot/__main__.py
@@ -1,9 +1,40 @@
 import config
 import media
 from InvadedRobot import bot
+from InvadedRobot.helpers.status import status
+from InvadedRobot.helpers.scandb import is_scan_user
 from pyrogram import filters
+from pyrogram.types import *
+from pyrogram import enums
+
+PM_PHOTO = "https://telegra.ph/file/e9a7b101e8fcc7e6b7381.jpg"
 
 
+PM_START_TEXT = """
+`Hello There I Am` `I⊃：INVΛ⊃≡⊃` `The Judgement Enforcing System`
+**Invaded Analysis Report :-**
+ ➛ **User:** {}
+ ➛ **ID:** `{}`
+ ➛ **Is Restricted:** `No`
+ ➛ **Status:** `{}`
+ ➛ **Crime Coefficient:** `Under - 100`
+"""
+
+@bot.on_message(filters.command("start"))
+async def start(_, message):
+   user_id = message.from_user.id
+   if message.chat.type == enums.ChatType.PRIVATE:
+        kk = await m.reply(text="`Analyzing The User`")
+        await asyncio.sleep(2)
+        mm = await kk.edit_text("`...`")
+        await asyncio.sleep(2)
+        ll = await mm.edit_text("`Processing...`")
+        await asyncio.sleep(3)
+        await ll.delete()
+        is_rank = await status(user_id)
+        is_scan = await is_scan_user(user_id)
+        mention = message.from_user.mention
+        await message.reply_photo(PM_PHOTO, caption=PM_START_TEXT.format(mention, user_id,is_scan, is_rank))
 
 
 if __name__ == "__main__":
