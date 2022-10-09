339f797c7673bc3cb4026dc69020b4d201ec32a5
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-09 11:06:45 +0530

    Create ranksdb.py

---
+++ InvadedRobot/helpers/ranksdb.py
@@ -0,0 +1,24 @@
+from InvadedRobot import mongodb
+
+ranksdb = mongodb.rankuserdb
+
+
+async def get_rankusers() -> list:
+    rank_users = await ranksdb.find_one({"user_id": "user_id"})
+    if not rank_users:
+        return []
+    return rank_users["rank_users"]
+
+async def add_rank(user_id: int):
+      rank_users = await get_rankusers()
+      rank_users.append(user_id)
+      await ranksdb.update_one(
+        {"user_id": "user_id"}, {"$set": {"rank_users": rank_users}}, upsert=True)
+      return True  
+
+async def remove_rank(user_id: int):
+       rank_users = await get_rankusers()
+       rank_users.remove(user_id)
+       await ranksdb.update_one(
+        {"user_id": "user_id"}, {"$set": {"rank_users": rank_users}}, upsert=True)
+       return True
