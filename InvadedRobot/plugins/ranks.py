bbd06d6900bec702c8785c39e65dc2a2b5b4579c
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-09 22:32:08 +0530

    Update ranks.py

--- InvadedRobot/plugins/ranks.py
+++ InvadedRobot/plugins/ranks.py
@@ -51,7 +51,7 @@ InlineKeyboardButton("promote to Troops", callback_data=f"promote_to_troops:{use
            if user_id in (await RANK_USERS()):
               await message.reply_text("`the user is Invaded you can demote Troops or Civilian`",
               reply_markup=InlineKeyboardMarkup([[
-InlineKeyboardButton("demoet to troops", callback_data=f"demote_to_troops:{user_id}"),],[
+InlineKeyboardButton("demoet to Troops", callback_data=f"demote_to_troops:{user_id}"),],[
 InlineKeyboardButton("demoet to Civilian", callback_data=f"demote_to_civilian:{user_id}")]]))
 
            elif user_id in (await TROOPS_USERS()):
@@ -75,12 +75,57 @@ async def demote_to_civilian(_, query):
           return await query.answer("Only work dev users.", show_alert=True)
       elif user_id in (await RANK_USERS()):
            await remove_rank(user_id)
-           await query.message.edit("`Successfully Demoted to Civilian!`")
+           await query.message.edit("`Successfully Invaded user Demoted to Civilian!`")
       elif user_id in (await TROOPS_USERS()):
            await remove_troop(user_id)
-           await query.message.edit("`Successfully Demoted to Civilian!`")
+           await query.message.edit("`Successfully troop user Demoted to Civilian!`")
    except Exception as e:
        await query.message.reply_photo(media.ERROR_IMG, caption=e)
+
+
+@bot.on_callback_query(filters.regex("demote_to_troops"))
+async def demote_to_troops(_, query):
+   user_id = int(query.data.split(":")[1])
+   try:
+      if not query.from_user.id in config.DEVS:
+          return await query.answer("Only work dev users.", show_alert=True)
+      elif user_id in (await RANK_USERS()):
+           await remove_rank(user_id)
+           await add_troop(user_id)
+           await query.message.edit("`Successfully Invaded user Demoted to Troops!`")
+   except Exception as e:
+       await query.message.reply_photo(media.ERROR_IMG, caption=e)
+
+
+@bot.on_callback_query(filters.regex("promote_to_troops"))
+async def promote_to_troops(_, query):
+   user_id = int(query.data.split(":")[1])
+   try:
+      if not query.from_user.id in config.DEVS:
+          return await query.answer("Only work dev users.", show_alert=True)
+      else:
+           await add_troop(user_id)
+           await query.message.edit("`Successfully Civilian promote to Troops!`")
+   except Exception as e:
+       await query.message.reply_photo(media.ERROR_IMG, caption=e)
+
+@bot.on_callback_query(filters.regex("promote_to_invaded"))
+async def promote_to_invaded(_, query):
+   user_id = int(query.data.split(":")[1])
+   try:
+      if not query.from_user.id in config.DEVS:
+          return await query.answer("Only work dev users.", show_alert=True)
+      elif user_id in (await TROOPS_USERS()):
+            await remove_troop(user_id)
+            await add_rank(user_id)
+            await query.message.edit("`Successfully Troop user Promoted to Invaded`")
+      else:
+          await add_rank(user_id)
+          await query.message.edit("`Successfully Civilian Promoted to Invaded`")
+   except Exception as e:
+       await query.message.reply_photo(media.ERROR_IMG, caption=e)
+
+
 
 
 
