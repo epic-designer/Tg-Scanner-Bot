108cd857c9533e1074a7ea15c79fe42749ebb431
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-09 21:11:32 +0530

    Update ranks.py

--- InvadedRobot/plugins/ranks.py
+++ InvadedRobot/plugins/ranks.py
@@ -1,5 +1,6 @@
 import config
 import asyncio
+import media
 
 from pyrogram import filters
 from pyrogram.types import *
@@ -17,9 +18,10 @@ async def rank(_, message):
     user_id = message.from_user.id
     if not user_id in config.DEVS:
         msg = await message.reply_text("`only devs can access this.`")
-        await asyncio.sleep(5)
+        await asyncio.sleep(10)
         await msg.delete()
     elif reply:
+       try:
            user_id = int(reply.from_user.id)
            if user_id in (await RANK_USERS()):
               await message.reply_text("`the user is Invaded you can demote Troops or Civilian`",
@@ -38,4 +40,33 @@ InlineKeyboardButton("demoet to Civilian", callback_data=f"demote_to_civilian:{u
 InlineKeyboardButton("promote to Invaded", callback_data=f"promote_to_invaded:{user_id}"),],[
 InlineKeyboardButton("promote to Troops", callback_data=f"promote_to_troops:{user_id}")]]))
            
+       except Exception as e;
+          await message.reply_photo(photo=media.ERROR_IMG,caption=e)
+
+    elif not reply:
+           if len(message.command) <2:
+                 msg = await message.reply_text("`you need to use correct formatting.`")
+                 await asyncio.sleep(10)
+                 await msg.delete() 
+           user_id = int(message.text.split("-u")[1])
+           if user_id in (await RANK_USERS()):
+              await message.reply_text("`the user is Invaded you can demote Troops or Civilian`",
+              reply_markup=InlineKeyboardMarkup([[
+InlineKeyboardButton("demoet to troops", callback_data=f"demote_to_troops:{user_id}"),],[
+InlineKeyboardButton("demoet to Civilian", callback_data=f"demote_to_civilian:{user_id}")]]))
+
+           elif user_id in (await TROOPS_USERS()):
+              await message.reply_text("`the user is Troop you can promote to Invaded or demote to Civilian`",
+              reply_markup=InlineKeyboardMarkup([[
+InlineKeyboardButton("promote to Invaded", callback_data=f"promote_to_invaded:{user_id}"),],[
+InlineKeyboardButton("demoet to Civilian", callback_data=f"demote_to_civilian:{user_id}")]]))
+           else:
+              await message.reply_text("`the user is Civilian you can promote to Invaded or promote to troops`",
+              reply_markup=InlineKeyboardMarkup([[
+InlineKeyboardButton("promote to Invaded", callback_data=f"promote_to_invaded:{user_id}"),],[
+InlineKeyboardButton("promote to Troops", callback_data=f"promote_to_troops:{user_id}")]]))
+           
+
+
+
 
