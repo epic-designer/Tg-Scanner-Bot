d72e8490748b0cc87e881a8d183f7130e56bde33
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-09 15:38:06 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,2 +1,4 @@
 INVADED_IMG = "https://telegra.ph/file/36e97d2d94e05be5d10b5.jpg"
 ERROR_IMG = "https://telegra.ph/file/f21e5445b3d0897f63f3d.jpg"
+PM_PHOTO = "https://telegra.ph/file/e9a7b101e8fcc7e6b7381.jpg"
+
