501bd7b63d20d393a9872437ab05d60eac815a7a
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-09 20:13:40 +0530

    Update rank.py

--- InvadedRobot/rank.py
+++ InvadedRobot/rank.py
@@ -1,10 +1,11 @@
 "Function To Get Rank Users For Special Access"
 from InvadedRobot.helpers.ranksdb import get_rankusers
-from InvadedRobot.helpers.troopdb import get_troops
+from InvadedRobot.helpers.troopsdb import get_troops
 
 async def RANK_USERS():
      list = (await get_rankusers())
      return list
-async def troopS():
+
+async def TROOP_USERS():
       list = (await get_troops())
       return list
