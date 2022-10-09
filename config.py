2dc21610de7d9e70b6f88e4e95c39bbe28bef9ad
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-09 12:43:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ COMMANDS = [".","?","/","!","$","#","~",]
 LOG_CHANNEL_ID = -1001558495242
 GROUP_URL = "https://t.me/InvadedSupport"
 DB_URL = "mongodb+srv://Bave999:Bave999@cluster0.1aheaa1.mongodb.net/?retryWrites=true&w=majority"
+SESSION = "BQDqqZsAvYDCvPS6eGggdblJT-ZQVmoi_hgL65ngYSZelZ825e4R0R_ie7BAMeDYb3rjmiBp8nZSuWGID5nZd1Rf9kdnA3eOZJAKhiCO3renEe2L80vTM3KPvrLzp7yPR0yK14YuIFUPuQZo1wFkxxCl--wOxtAKbCXSjQ7IQHrFI8h_ahTjEqMd4F0PpYGYr7HFgXQRvDvtUVuJ0C3cEO-wOsdi9mT82MO4GTBrAFBT9MU-HHb3cYpdOB1_G-tzOGOPxG65r-OCjrqUvG1LsoIPf1ruEVWvi6bP4cdg5pDwGiXGMn7yORW8a8LynG450K3wqKZa9y9CByL42I-HLlResh6p6gAAAAFCPvD-AA"
