3b9e86cd44bfa86c250990542b86d0bd25cfc017
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-09 14:51:48 +0530

    Update __main__.py

--- InvadedRobot/__main__.py
+++ InvadedRobot/__main__.py
@@ -9,5 +9,5 @@ from pyrogram import filters
 if __name__ == "__main__":
      bot.run()
      with bot:
-        bot.send_photo(config.LOG_GROUP_ID,photo=(media.INVADED_IMG),caption="<b>I'm Awake Already!</b>",
+        bot.send_photo(chat_id=config.LOG_GROUP_ID,photo=(media.INVADED_IMG),caption="<b>I'm Awake Already!</b>",
           reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("𝗚𝗥𝗢𝗨𝗣!",url=f"{config.GROUP_URL}")]]))
