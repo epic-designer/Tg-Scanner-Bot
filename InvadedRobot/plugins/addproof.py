1c6e08bcb085e48afa7f345a8c65b79a87396b34
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-09 11:21:23 +0530

    Update addproof.py

--- InvadedRobot/plugins/addproof.py
+++ InvadedRobot/plugins/addproof.py
@@ -1,6 +1,4 @@
-
 import config
-
 
 from pyrogram import filters
 from pyrogram.types import *
@@ -12,25 +10,25 @@ get_scan_users, add_scan_user, get_scan_user,
 
 from InvadedRobot.rank import RANK_USERS
 
-
-@bot.on_message(filters.command("addproof",config.COMMANDS))
+@bot.on_message(filters.command("addproof", config.COMMANDS))
 async def addproof(_, message):
       reply = message.reply_to_message
       date = message.date
-      msg = await message.reply_text("`adding proof...`")
+      msg = await message.reply_text("`Adding Proof...`")
       if not message.from_user.id in (await RANK_USERS()):
-           return await msg.edit("`you don't have enough rights to use me.`")
+           return await msg.edit("`You Don't Have Enough Rights To Use Me...`")
       elif len(message.command) <2:
-           return await msg.edit("`use a correct format for add proof.`")
+           return await msg.edit("`Use A Correct Format For Adding The Proof...`")
       elif not reply or not reply.media:  
-            return await msg.edit("`reply to media for save proofs.`")
+            return await msg.edit("`Reply To A Media To Save Proof...`")
       elif reply:
           try:           
              user_id = int(message.text.split("-u")[1])
              if not user_id in (await get_scan_users()):
-                 return await msg.edit("`this user not a scanned user to add proof.`")
+                 return await msg.edit("`This User Is Not Scanned The User Must To Been To Add Proof...`")
 
              await update_scan_proof(user_id, message)
-             await msg.edit("`Successfully proof added!`")   
+             await msg.edit("`Successfully Added Proof!`")   
           except Exception as e:
-              await msg.edit(str(e))
+              await msg.delete()
+              await message.reply_photo("https://telegra.ph/file/f21e5445b3d0897f63f3d.jpg", caption=e)
