1fc8cc33624052efacd29fa54a0c858c43311365
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-09 20:49:54 +0530

    Create ranks.py

---
+++ InvadedRobot/plugins/ranks.py
@@ -0,0 +1,48 @@
+import config
+import asyncio
+
+from pyrogram import filters
+from pyrogram.types import *
+
+
+from InvadedRobot import bot
+from InvadedRobot.rank import RANK_USERS , TROOP_USERS
+from InvadedRobot.helpers.ranksdb import *
+from InvadedRobot.helpers.troopsdb import *
+
+
+InlineKeyboardMarkup([[
+InlineKeyboardButton("promote to Invaded", callback_data=]])
+
+@bot.on_message(filters.command("setrank"))
+async def rank(_, message):
+    reply = message.reply_to_message
+    user_id = message.from_user.id
+    if not user_id in DEVS:
+        msg = await message.reply_text("`only devs can access this.`")
+        asyncio.sleep(5)
+        await msg.delete()
+    elif len(message.command) <2:
+        msg = await message.reply_text("`you need to use correct formatting.`")
+        asyncio.sleep(5)
+        await msg.delete()
+    elif reply:
+           user_id = int(reply.from_user.id)
+           if user_id in (await RANK_USER()):
+              await message.reply_text("`the user is Invaded you can demote Troops or Civilian`",
+              reply_markup=InlineKeyboardMarkup([[
+InlineKeyboardButton("demoet to troops", callback_data=f"demote_to_troops:{user_id}"),],[
+InlineKeyboardButton("demoet to troops", callback_data=f"demote_to_civilian:{user_id}")]]))
+
+           elif user_id in (await TROOPS_USERS()):
+              await message.reply_text("`the user is Troop you can promote to Invaded or demote to Civilian`",
+              reply_markup=InlineKeyboardMarkup([[
+InlineKeyboardButton("promote to Invaded", callback_data=f"promote_to_invaded:{user_id}"),],[
+InlineKeyboardButton("demoet to Civilian", callback_data=f"demote_to_civilian:{user_id}")]]))
+           else:
+              await message.reply_text("`the user is Civilian you can promote to Invaded or promote to troops`",
+              reply_markup=InlineKeyboardMarkup([[
+InlineKeyboardButton("promote to Invaded", callback_data=f"promote_to_invaded:{user_id}"),],[
+InlineKeyboardButton("demoet to Civilian", callback_data=f"demote_to_civilian:{user_id}")]]))
+           
+
