6960f6130e9373602a9f29e269b5190c2e18059d
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-09 15:37:32 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -13,3 +13,21 @@ CHECK_TEXT = """
 
 × **Scan Processed Time And Date**: `{}`
 """
+
+PM_START_TEXT = """
+`Hello There I Am` `I⊃：INVΛ⊃≡⊃` `The Judgement Enforcing System`
+**Invaded Analysis Report :-**
+ ➛ User: {}
+ ➛ ID: `{}`
+ ➛ Is Restricted: `{}`
+ ➛ Status: `{}`
+"""
+
+GROUP_START_TEXT = """
+`Hello There I Am` `I⊃：INVΛ⊃≡⊃` `The Judgement Enforcing System`
+Invaded Analysis Report :-
+ ➛ Group: `{}`
+ ➛ ID: `{}`
+ ➛ Members Count: `{}`
+ ➛ Message Count: `{}`
+"""
