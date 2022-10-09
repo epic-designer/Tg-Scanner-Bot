962f1340cef620a10208c19dab1143123205cd67
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-09 20:12:12 +0530

    Update __init__.py

--- InvadedRobot/__init__.py
+++ InvadedRobot/__init__.py
@@ -30,7 +30,7 @@ inv = Client(name="Invaded",
              api_hash=config.API_HASH,
              session_string=config.SESSION)
 
-inv.start()
+
 
 # mongodb from pymongo #
 pymongo = MongoClient(config.DB_URL)
