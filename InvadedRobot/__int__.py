74dba17374820dcd324fe07d5262fd64a33bce78
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-09 10:42:29 +0530

    Update __int__.py

--- InvadedRobot/__int__.py
+++ InvadedRobot/__int__.py
@@ -14,19 +14,13 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-### Add This Vars On Your Deploying App. ###
-API_ID = os.environ.get("API_ID", 7126006)
-API_HASH = os.environ.get("API_HASH", "f92b05be529835381859ead64a195fa2")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5515671520:AAFcuSq058VM8QmShpweRs1p0u0U5pWHIH8")
-
 
 #pyrogram bot client start
-
 plugins = dict(root="InvadedRobot")
 bot = Client(name=str(config.USERNAME), 
-             api_id=API_ID, 
-             api_hash=API_HASH,
-             bot_token=BOT_TOKEN,
+             api_id=config.API_ID, 
+             api_hash=config.API_HASH,
+             bot_token=config.BOT_TOKEN,
              plugins=plugins)
 
 # mongodb from pymongo #
