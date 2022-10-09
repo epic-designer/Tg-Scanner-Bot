40ddddd8371f9a0e7969eb2a49d3ae7a7a117575
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-09 10:57:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 USERNAME = "InvadedRobot" 
-BOT = 5459099380
+BOT_ID = 5459099380
 API_ID = 583311
 API_HASH = "c67090d2c0a37d207385d04d9640045c"
 BOT_TOKEN = "5459099380:AAGd6YsZDwkkeHaLZ7J6xY3xVcHZKbVsqQQ"
-GROUP_ID = -1001644486123
+LOG_GROUP_ID = -1001644486123
+LOG_CHANNEL_ID = -1001558495242
 GROUP_URL = "https://t.me/InvadedSupport"
 DB_URL = "mongodb+srv://Invaded:Invaded999@cluster0.y7ilrwk.mongodb.net/?retryWrites=true&w=majority"
