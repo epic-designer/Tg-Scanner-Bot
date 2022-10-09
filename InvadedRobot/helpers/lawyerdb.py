283826cd661cd013071b3be51dd3e34d55778d47
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-09 16:29:47 +0530

    Create lawyerdb.py
    Done lawyers db

---
+++ InvadedRobot/helpers/lawyerdb.py
@@ -0,0 +1,37 @@
+from InvadedRobot import mongodb
+
+lawyersdb = mongodb.lawyerdb
+
+async def get_lawyers() -> list:
+
+    rank_users = await lawyersdb.find_one({"user_id": "user_id"})
+
+    if not lawyer_users:
+
+        return []
+
+    return lawyer_users["lawyer_users"]
+
+async def add_lawyer(user_id: int):
+
+      lawyer_users = await get_lawyers()
+
+      lawyer_users.append(user_id)
+
+      await lawyersdb.update_one(
+
+        {"user_id": "user_id"}, {"$set": {"lawyer_users": lawyer_users}}, upsert=True)
+
+      return True  
+
+async def remove_lawyer(user_id: int):
+
+       lawyer_users = await get_lawyers()
+
+       lwayer_users.remove(user_id)
+
+       await lawyersdb.update_one(
+
+        {"user_id": "user_id"}, {"$set": {"lawyer_users": lawyer_users}}, upsert=True)
+
+       return True
