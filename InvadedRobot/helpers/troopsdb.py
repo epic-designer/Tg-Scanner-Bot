3824c415a0c7eda30d79376183db13068ca4bc2f
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-10 16:40:58 +0530

    Update troopsdb.py

--- InvadedRobot/helpers/troopsdb.py
+++ InvadedRobot/helpers/troopsdb.py
@@ -3,35 +3,21 @@ from InvadedRobot import mongodb
 troopsdb = mongodb.troopdb
 
 async def get_troops() -> list:
-
     troop_users = await troopsdb.find_one({"user_id": "user_id"})
-
     if not troop_users:
-
         return []
-
     return troop_users["troop_users"]
 
 async def add_troop(user_id: int):
-
       troop_users = await get_troops()
-
       troop_users.append(user_id)
-
       await troopsdb.update_one(
-
         {"user_id": "user_id"}, {"$set": {"troop_users": troop_users}}, upsert=True)
-
       return True  
 
 async def remove_troop(user_id: int):
-
        troop_users = await get_troops()
-
        troop_users.remove(user_id)
-
        await troopsdb.update_one(
-
         {"user_id": "user_id"}, {"$set": {"troop_users": troop_users}}, upsert=True)
-
        return True
