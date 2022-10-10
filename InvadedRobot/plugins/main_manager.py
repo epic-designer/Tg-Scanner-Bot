3b48ab51ee1748b46b8f5fe93c8393b09d537d70
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-10 20:56:49 +0530

    Update main_manager.py

--- InvadedRobot/plugins/main_manager.py
+++ InvadedRobot/plugins/main_manager.py
@@ -30,14 +30,17 @@ async def scan(_, message):
             reason = message.text.split("-r")[1].split("-p")[0]
             proof = message.text.split("-p")[1]
             mention = f"[{user_id}](tg://user?id={user_id})"
-            uid = message.from_user.id
             if (await is_scan_user(user_id)) == True:
                  await msg.edit("`The user already scanned in Invaded no need request.`")
             else:
+                t_id = message.from_user.id
+                s_id = int(reply.from_user.id)
+                r = message.text.split("-r")[1].split("-p")[0]
+                p = message.text.split("-p")[1]
                 await bot.send_message(config.REPORT_GROUP, text=strings.REQUEST_SCAN.format(message.from_user.mention, mention, reason, proof, date),reply_markup=InlineKeyboardMarkup([[
-InlineKeyboardButton("Approve Scan",callback_data=f"approve_scan:{uid}:{user_id}:{reason}:{proof}:{date}"),
+InlineKeyboardButton("Approve Scan",callback_data=f"approve_scan:{t_id}:{s_id}:{r}:{p}:{date}"),
 ],[
-InlineKeyboardButton("Disapprove Scan",callback_data=f"disapprove_scan:{uid}")]]))
+InlineKeyboardButton("Disapprove Scan",callback_data=f"disapprove_scan:{t_id}")]]))
                 await msg.edit("the user Successfully requested to Invaded")
         except Exception as e:
             await msg.delete()
