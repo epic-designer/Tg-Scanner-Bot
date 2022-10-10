89090ab37c55632c387e6b19c275e173a9bae42c
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-10 09:01:47 +0530

    Update gbanstuffs.py

--- InvadedRobot/gbanstuffs.py
+++ InvadedRobot/gbanstuffs.py
@@ -5,7 +5,7 @@ from InvadedRobot import inv
 
 async def gban(troop, target, reason, msg_id, approved_by):
    logs = config.GBAN_MSG_LOGS
-   await inv.send_message(LOG_CHANNEL_ID, strings.scan_approved_string.format(enforcer=enforcer, scam=target, approved_by= f"[{approved_by.first_name}](tg://user?id={approved_by.id})"))
-   await inv.send_message(logs, f"/gban [{target}](tg://user?id={target}) {reason} // By {enforcer} | #{msg_id}")
-   await inv.send_message(logs, f"/fban [{target}](tg://user?id={target}) {reason} // By {enforcer} | #{msg_id}") 
+   await inv.send_message(LOG_CHANNEL_ID, strings.scan_approved_string.format(troop=troop, scam=target, approved_by= f"[{approved_by.first_name}](tg://user?id={approved_by.id})"))
+   await inv.send_message(logs, f"/gban [{target}](tg://user?id={target}) {reason} // By {troop} | #{msg_id}")
+   await inv.send_message(logs, f"/fban [{target}](tg://user?id={target}) {reason} // By {troop} | #{msg_id}") 
    return True
