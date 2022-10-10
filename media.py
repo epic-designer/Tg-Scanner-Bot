a83da3a3b9ddda942d7e9e37e57cfd13a0c44bf3
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-10 18:25:11 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,4 +1,4 @@
-INVADED_IMG = "https://telegra.ph/file/36e97d2d94e05be5d10b5.jpg"
+INVADED_VID = "https://telegra.ph/file/a69feed5d48f6554ac47a.mp4"
 ERROR_IMG = "https://telegra.ph/file/f21e5445b3d0897f63f3d.jpg"
 PM_PHOTO = "https://telegra.ph/file/e9a7b101e8fcc7e6b7381.jpg"
 
