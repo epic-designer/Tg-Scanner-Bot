939cc06e162d74fc7e3ab6385e65005babde1401
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-10 16:35:03 +0530

    Update proof.py

--- InvadedRobot/plugins/proof.py
+++ InvadedRobot/plugins/proof.py
@@ -1,14 +1,10 @@
 import config
 
-from pyrogram import filters
+from pyrogram import *
 from pyrogram.types import *
-from InvadedRobot import bot
-from InvadedRobot.helpers.scandb import (
-get_scan_users, add_scan_user, get_scan_user,
- is_scan_user, remove_scan_user, update_scan_reason, update_scan_proof
-)
-
-from InvadedRobot.rank import RANK_USERS
+from InvadedRobot import *
+from InvadedRobot.helpers.scandb import *
+from InvadedRobot.rank import *
 
 @bot.on_message(filters.command("proof", config.COMMANDS))
 async def proof(_, message):
