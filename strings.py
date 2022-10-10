8abd5a6ae3b58f3653e765cf8ee55e45efb3039e
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-10 16:56:07 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,5 +1,5 @@
 SCAN_TEXT = """
-#LethalEliminator
+**#LethalEliminator**
 **• Commander** : `{}`
 **• Target User** : {}
 **• Reason** : `{}`
@@ -36,22 +36,22 @@ CHECK_TEXT = """
 
 PM_START_TEXT = """
 `Hello There I Am` `I⊃：INVΛ⊃≡⊃` `The Judgement Enforcing System`
-**Invaded Analysis Report :-**
 
- ➛ User: {}
- ➛ ID: `{}`
- ➛ Is Restricted: `{}`
- ➛ Status: `{}`
+**Invaded Analysis Report :-**
+**➛ User:** {}
+**➛ ID:** `{}`
+**➛ Is Restricted:** `{}`
+**➛ Status:** `{}`
 """
 
 GROUP_START_TEXT = """
 `Hello There I Am` `I⊃：INVΛ⊃≡⊃` `The Judgement Enforcing System`
-Invaded Analysis Report :-
 
- ➛ Group: `{}`
- ➛ ID: `{}`
- ➛ Members Count: `{}`
- ➛ Message Count: `{}`
+**Invaded Analysis Report :-**
+**➛ Group:** `{}`
+**➛ ID:** `{}`
+**➛ Members Count:** `{}`
+**➛ Message Count:** `{}`
 """
 
 FORMAT_TEXT = """
