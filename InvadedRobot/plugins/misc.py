013277aed68a4a367266d5cd8cf48f8c8eb33123
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-10 16:31:12 +0530

    Delete misc.py

--- InvadedRobot/plugins/misc.py
+++
@@ -1,9 +0,0 @@
-import config
-import strings
-
-from pyrogram import filters
-from InvadedRobot import bot
-
-@bot.on_message(filters.command(["formatting","format"],config.COMMANDS))
-async def formatting(_, message):
-    await message.reply_text(strings.FORMAT_TEXT)
