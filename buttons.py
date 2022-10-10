333f7e6a9fe3142b561fcdb2bd84bd096b1aee97
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-10 10:12:51 +0530

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -1,14 +1,16 @@
 from pyrogram.types import *
 
-HELP_MENU_1 = InlineKeyboardMarkup([[InlineKeyboardButton("𝗥𝗔𝗡𝗞 𝗨𝗦𝗘𝗥𝗦",callback_data="rankusers")
-],[InlineKeyboardButton("𝗛𝗢𝗪 𝗜 𝗥𝗘𝗣𝗢𝗥𝗧",callback_data="how_i_report"),
-],[InlineKeyboardButton("𝗛𝗢𝗪 𝗜 𝗥𝗘𝗩𝗘𝗥𝗧",callback_data="how_i_revert"),
+HELP_MENU_1 = InlineKeyboardMarkup([[InlineKeyboardButton("RANK USER",callback_data="rankusers")
+],[InlineKeyboardButton("HOW I REPORT",callback_data="how_i_report"),
+],[InlineKeyboardButton("HOW I REVERT",callback_data="how_i_revert"),
 ],[
-InlineKeyboardButton("𝗠𝗢𝗥𝗘 𝗛𝗘𝗟𝗣",callback_data="help_2"),
-InlineKeyboardButton("𝗖𝗟𝗢𝗦𝗘 𝗠𝗘𝗡𝗨",callback_data="close_menu")]])
+InlineKeyboardButton("SKIP HELP",callback_data="help_2"),
+InlineKeyboardButton("CLOSE MENU",callback_data="close_menu")]])
 
 HELP_MENU_2 = InlineKeyboardMarkup([[
-InlineKeyboardButton("𝗛𝗢𝗪 𝗜 𝗦𝗖𝗔𝗡",callback_data="how_i_scan"),
+InlineKeyboardButton("HOW I SCAN",callback_data="how_i_scan"),
 ],[
-InlineKeyboardButton("𝗛𝗢𝗪 𝗜 𝗖𝗛𝗘𝗖𝗞",callback_data="how_i_check"),
-],[InlineKeyboardButton("𝗖𝗟𝗢𝗦𝗘 𝗠𝗘𝗡𝗨",callback_data="close_menu")]])
+InlineKeyboardButton("HOW I CHECK",callback_data="how_i_check"),
+],[
+InlineKeyboardButton("BACK HELP",callback_data="help_1"),
+InlineKeyboardButton("CLOSE MENU",callback_data="close_menu")]])
