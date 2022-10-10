1afa3c14df64797dac44b3536f0e21a99a70bd5b
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-10 09:45:59 +0530

    Update status.py

--- InvadedRobot/helpers/status.py
+++ InvadedRobot/helpers/status.py
@@ -4,8 +4,8 @@ from InvadedRobot.rank import RANK_USERS, TROOP_USERS
 async def status(user_id: int):
      if user_id in (await RANK_USERS()) and user_id in config.DEVS:
          return "Invader"
-     if user_id in (await RANK_USERS()):
+     elif user_id in (await RANK_USERS()):
          return "Commander"
-     if user_id in (await TROOP_USERS()):
+     elif user_id in (await TROOP_USERS()):
          return "Troop"
      return "Civilian"
