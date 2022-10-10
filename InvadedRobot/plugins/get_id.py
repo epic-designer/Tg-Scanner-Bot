629614c1c9f0fed3f4119369fd182e05de2376a7
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-10 10:59:38 +0530

    Create get_id.py

---
+++ InvadedRobot/plugins/get_id.py
@@ -0,0 +1,44 @@
+from InvadedRobot import bot
+from PIL import Image, ImageDraw, ImageFont
+import os
+import random
+import requests 
+import io
+from io import BytesIO
+
+
+@bot.on_message(filters.command("get_id"))
+async def image_maker(_, message) -> None:
+    replied_user = message.reply_to_message
+    # Download profile photo
+    await bot.download_profile_photo(
+        replied_user.from_user.id, file="user.png", download_big=True
+    )
+    k = ["https://telegra.ph/file/853962e208ec379284185.jpg", ""]
+    y = random.choice(k)
+    # open id photo
+    id_template = Image.open(io.BytesIO(requests.get(y).content))
+    # resize user photo to fit box in id template
+    user_photo = user_photo.resize((1159, 1241))
+    # put image in position
+    id_template.paste(user_photo, (1003, 641))
+    # postion on where to draw text
+    draw = ImageDraw.Draw(id_template)
+    color = "rgb(0, 0, 0)"  # black
+    font = ImageFont.truetype("font.ttf", size=80)
+    font2 = ImageFont.truetype("font2.ttf", size=100)
+    # put text in image
+    draw.text(
+        (1000, 460),
+        replied_user.from_user.first_name.replace("\u2060", ""),
+        fill=color,
+        font=font2,
+    )
+    draw.text((393, 50), str(replied_user.from_user.id), fill=color, font=font)
+    id_template.save("user_id.png")
+    await bot.send_file(
+        message.chat.id,
+        file="user_id.png",
+        caption="Generated User ID"
+    )
+    os.remove("user_id.png")
