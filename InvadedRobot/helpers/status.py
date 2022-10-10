433d51685f0374a8ea0a3d644fbb729359a9e390
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-10 16:41:17 +0530

    Update status.py

--- InvadedRobot/helpers/status.py
+++ InvadedRobot/helpers/status.py
@@ -1,5 +1,5 @@
 import config
-from InvadedRobot.rank import RANK_USERS, TROOP_USERS
+from InvadedRobot.rank import *
 
 async def status(user_id: int):
      if user_id in (await RANK_USERS()) and user_id in config.DEVS:
