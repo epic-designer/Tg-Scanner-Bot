baaa3dd4f8d6d52cbe8c853c1136650ed781e3a0
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-10 16:44:03 +0530

    Update __init__.py

--- InvadedRobot/__init__.py
+++ InvadedRobot/__init__.py
@@ -9,21 +9,25 @@ from pymongo import *
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[logging.FileHandler('logs.txt'),
-              logging.StreamHandler()],
-    level=logging.INFO)
+              logging.StreamHandler()], level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
+
 plugins = dict(root="InvadedRobot")
+
 bot = Client(name=str(config.USERNAME), 
              api_id=config.API_ID, 
              api_hash=config.API_HASH,
              bot_token=config.BOT_TOKEN,
              plugins=plugins)
+
 inv = Client(name="Invaded", 
              api_id=config.APP_ID, 
              api_hash=config.APP_HASH,
              session_string=config.SESSION)
+
 #inv.start()
 pymongo = MongoClient(config.DB_URL)
 pymongodb = pymongo.bot
+
 mongo = AsyncIOMotorClient(config.DB_URL)
 mongodb = mongo.bot 
