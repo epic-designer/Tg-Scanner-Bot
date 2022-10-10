77611a5192824b0d8cfde7de48dcf05041df135f
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-10 23:28:43 +0530

    Create telegraph.py

---
+++ InvadedRobot/helpers/telegraph.py
@@ -0,0 +1,22 @@
+from telegraph import upload_file
+from InvadedRobot import bot
+
+async def telegraph(message):
+        if not message.reply_to_message and not message.reply_to_message.media:
+            await message.reply_text("`reply to photo or document to upload telegraph note: it file size almost lessen 6mb`")
+        else:
+           path = await message.reply_to_message.download()
+           telegraph = upload_file(path)
+           for file_id in telegraph:
+               url = "https://telegra.ph" + file_id
+           if url.endswith("mp4"):
+                return await message.reply_video(video=url,caption=url)
+           elif url.endswith("jpg"):
+                return await message.reply_photo(photo=url,caption=url)
+           elif url.endswith("gif"):
+                return await message.reply_animation(animation=url,caption=url)
+
+
+@bot.on_message(filters.command("tm"))
+async def tm(_, message):
+       await telegraph(message)
