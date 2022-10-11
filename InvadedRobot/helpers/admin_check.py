2179d1cddb39eb038cd29462cb3b235bd1d552f6
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-11 23:34:09 +0530

    Update admin_check.py

--- InvadedRobot/helpers/admin_check.py
+++ InvadedRobot/helpers/admin_check.py
@@ -2,8 +2,8 @@
 
 
 
-async def admin(user_id: int):
-       admin = await get_member(user_id)
+async def admin(chat_id: int, user_id: int):
+       admin = await bot.get_chat_member(chat_id, user_id)
        if admin.privileges:
            return True
        return False
