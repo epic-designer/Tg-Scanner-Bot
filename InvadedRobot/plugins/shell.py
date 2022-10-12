4d438901ac635b8426500561fa5f5277af295265
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-12 12:17:13 +0530

    Update shell.py

--- InvadedRobot/plugins/shell.py
+++ InvadedRobot/plugins/shell.py
@@ -18,7 +18,7 @@ async def edit_or_reply(message, **kwargs):
     await func(**{k: v for k, v in kwargs.items() if k in spec})
 
 @bot.on_message(
-    filters.command(["sh", "shell", "term", "terminal"]
+    filters.command(["sh", "shell", "term", "terminal"],
     config.COMMANDS),
     & filters.user(config.DEVS)
     & ~filters.forwarded
