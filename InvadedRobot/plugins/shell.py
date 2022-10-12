554264c7307c3cd278946fbd6c960f6ad846ca0e
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-12 11:53:52 +0530

    Create shell.py

---
+++ InvadedRobot/plugins/shell.py
@@ -0,0 +1,93 @@
+import os, re sys, config, traceback, subprocess
+
+from inspect import getfullargspec
+from pyrogram import filters
+from InvadedRobot import bot, parse_com
+
+def parse_com(com, key):
+  try:
+    r = com.split(key,1)[1]
+  except KeyError:
+    return None
+  r = (r.split(" ", 1)[1] if len(r.split()) >= 1 else None)
+  return r
+
+async def edit_or_reply(message, **kwargs):
+    func = message.edit_text if message.from_user.is_self else message.reply
+    spec = getfullargspec(func.__wrapped__).args
+    await func(**{k: v for k, v in kwargs.items() if k in spec})
+
+@bot.on_message(
+    filters.command("sh"),
+    config.COMMANDS
+    & filters.user(config.DEVS)
+    & ~filters.forwarded
+    & ~filters.via_bot
+)
+async def shellrunner(client, message):
+    if len(message.command) < 2:
+        return await edit_or_reply(
+            message, text="**Usage:**\n`/sh git pull`"
+        )
+    text = parse_com(message.text, "sh")
+    if "\n" in text:
+        code = text.split("\n")
+        output = ""
+        for x in code:
+            shell = re.split(
+                """ (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", x
+            )
+            try:
+                process = subprocess.Popen(
+                    shell,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                )
+            except Exception as err:
+                print(err)
+                await edit_or_reply(
+                    message, text=f"**ERROR:**\n```{err}```"
+                )
+            output += f"**{code}**\n"
+            output += process.stdout.read()[:-1].decode("utf-8")
+            output += "\n"
+    else:
+        shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", text)
+        for a in range(len(shell)):
+            shell[a] = shell[a].replace('"', "")
+        try:
+            process = subprocess.Popen(
+                shell,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+        except Exception as err:
+            print(err)
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            errors = traceback.format_exception(
+                etype=exc_type,
+                value=exc_obj,
+                tb=exc_tb,
+            )
+            return await edit_or_reply(
+                message, text=f"**ERROR:**\n```{''.join(errors)}```"
+            )
+        output = process.stdout.read()[:-1].decode("utf-8")
+    if str(output) == "\n":
+        output = None
+    if output:
+        if len(output) > 4096:
+            with open("output.txt", "w+") as file:
+                file.write(output)
+            await client.send_document(
+                message.chat.id,
+                "output.txt",
+                reply_to_message_id=message.message_id,
+                caption="`Output`",
+            )
+            return os.remove("output.txt")
+        await edit_or_reply(
+            message, text=f"**OUTPUT:**\n```{output}```"
+        )
+    else:
+        await edit_or_reply(message, text="**OUTPUT: **\n`No Output`")
