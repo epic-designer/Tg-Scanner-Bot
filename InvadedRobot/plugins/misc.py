24798ec65332ef1b3258456bf10ad71d0478996a
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-12 07:14:06 +0530

    Update misc.py

--- InvadedRobot/plugins/misc.py
+++ InvadedRobot/plugins/misc.py
@@ -1,9 +1,12 @@
 import config
+import strings
 
 from pyrogram import filters
-from InvadedRobot import bot
+from InvadedRobot import bot, inv
 from InvadedRobot.helpers.telegraph import telegraph
 
+
+@inv.on_message(filters.command(["tm","tgm","telegraph"],config.COMMANDS))
 @bot.on_message(filters.command(["tm","tgm","telegraph"],config.COMMANDS))
 async def tm(_, message):
        await telegraph(message)
