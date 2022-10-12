96722f0cce232916f01380c266ff1291d728f947
ＮΛＮＤＨΛ╰ » Ⓒ︎Ⓣ « ╮ <89440790+NandhaxD@users.noreply.github.com>
2022-10-12 20:49:01 +0530

    Update stats.py

--- InvadedRobot/plugins/stats.py
+++ InvadedRobot/plugins/stats.py
@@ -20,5 +20,5 @@ async def stats(_, message):
    else:
        total_scans = len(await get_scan_users())
        total_troop = len(await TROOP_USERS())
-       total_commander= len(await RANK_USERS())
-       return await message.reply(strings.STATS.format(total_troop,total_commanders,total_scans))
+       total_commander = len(await RANK_USERS())
+       return await message.reply(strings.STATS.format(total_troop,total_commander,total_scans))
