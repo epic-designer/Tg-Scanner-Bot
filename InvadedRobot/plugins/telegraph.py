0fdecda48cf9702ae0ea071420f6f12337c10939
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-12 11:27:44 +0530

    Update telegraph.py

--- InvadedRobot/plugins/telegraph.py
+++ InvadedRobot/plugins/telegraph.py
@@ -4,7 +4,6 @@ from pyrogram import filters
 from InvadedRobot import bot, inv
 from InvadedRobot.helpers.telegraph import telegraph
 
-
 @inv.on_message(filters.command(["tm","tgm","telegraph"],config.COMMANDS))
 @bot.on_message(filters.command(["tm","tgm","telegraph"],config.COMMANDS))
 async def tm(_, message):
@@ -12,6 +11,6 @@ async def tm(_, message):
 
 help_text = """
 **Here Is The Help For** `Telegraph`:-
-➛ `tm` `tgm` `telegraph` - Convert Media Into Telegraph Link
+➛ `tm`, `tgm`, `telegraph` - **Convert Media Into Telegraph Link**
 """
 __plugin_name__ = "telegraph"
