b6f8e36701c4bac05c0dde53e5610f195eb7b212
Aasf [ありがとう] <89995228+AasfCyberKing@users.noreply.github.com>
2022-10-13 17:48:04 +0530

    Update telegraph.py

--- InvadedRobot/helpers/telegraph.py
+++ InvadedRobot/helpers/telegraph.py
@@ -15,7 +15,7 @@ async def telegraph(message):
             url = "https://telegra.ph" + file_id
             end = datetime.now()
         ms = (end - start).seconds
-        caption = f"**dl speed**: `{ms}`\n\n{url}"
+        caption = f"**Download Speed**: `{ms}`\n\n{url}"
         if url.endswith("mp4"):
                 return await message.reply_video(video=url,caption=caption)
         elif url.endswith("jpg"):
