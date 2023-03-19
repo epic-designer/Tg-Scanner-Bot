e184d491582e51787da27b8d396301740ff6997e
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-03-19 15:35:48 +0530

    Delete Nandha.py

--- Nandha.py
+++
@@ -1,9 +0,0 @@
-
-x = "it's "
-y = "Me " #Youtuber
-z = "Nandha 🧑‍🏫" #Programme
-for x in range(1000): #Blogger
-    print(x + y + z) #logo & Video Design
-
-#Output thousand times 
-# print() "It's Me Nandha"
