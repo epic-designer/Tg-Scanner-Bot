c6c48bc49dd8dc4d18b696971c34f69c9877fb2e
epic-designer <87082611+epic-designer@users.noreply.github.com>
2023-07-18 12:00:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,16 @@ SESSION = "BQCGuUKkSZMfPqfszJD9tqvbrULmrxG5VC7llEyImHMpvsg9RtfrzlTsZScFIiY3hAgke
 APP_ID = 28122413
 APP_HASH = "750432c8e1b221f91fd2c93a92710093"
 BOT_TOKEN = "6339781130:AAEw2dHjSrkKaytBHDXBzr5Y9c2lkhZgLRw"
-LOG_GROUP_ID = -1001849819947
-USERNAME = "InvadedRobot" 
+LOG_GROUP_ID = -806319628
+USERNAME = "Un_ins_scanner_bot" 
 BOT_ID = 5459099380
 COMMANDS = [".","?","/","!","$","#","~",]
-LOG_CHANNEL_ID = -1001849819947
-REPORT_GROUP = -1001849819947
-GROUP_URL = "t.me/botsupportx"
-UPDATE_CHANNEL_URL = "t.me/botsupportx"
-SYSTEM_CHANNEL_URL = "t.me/botsupportx"
-LOG_CHANNEL_URL = "t.me/botsupportx"
-DB_URL = "mongodb+srv://nandhasigma:iqKZ5OnIBVcpZde2@cluster0.gt47zau.mongodb.net/?retryWrites=true&w=majority"
-DEVS = [5443243540]
-INV_CORE_1 = -1001849819947
+LOG_CHANNEL_ID = -806319628
+REPORT_GROUP = -806319628
+GROUP_URL = "https://t.me/+gj-hc2dhRbwzMGM1"
+UPDATE_CHANNEL_URL = "https://t.me/+gj-hc2dhRbwzMGM1"
+SYSTEM_CHANNEL_URL = "https://t.me/+gj-hc2dhRbwzMGM1"
+LOG_CHANNEL_URL = "https://t.me/+gj-hc2dhRbwzMGM1"
+DB_URL = "mongodb+srv://HoshinoAI:HoshinoV1@hoshinodb.cfany1w.mongodb.net/?retryWrites=true&w=majority"
+DEVS = [5443243540 5690711835 5715764478]
+INV_CORE_1 = -806319628
